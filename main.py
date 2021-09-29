6fd2ac8ebce86781559cb2225ee9b1cd8f5698a0
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2021-09-29 01:02:37 -0400

    A substantial rewrite of most of the code in the project

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import yfinance as yf
 import streamlit as st
 import datetime as dt
-
-# Hello! Welcome to my first project!
+# Hello! Welcome to my first project! Ideally, this code will change over time as I learn more about Python!
 
 st.set_page_config(
      page_title="Jordan's Stock Analyzer",
@@ -26,49 +25,76 @@ symbol = st.text_input("What ticker symbol would you like to analyze?: ", value=
 # be found, it will handle that error.
 try:
     if symbol != "":
-        st.write(f"You have chosen {symbol}. Here's some relevant information: ")
+        st.markdown(f"You have chosen **{symbol}**. Here's some relevant information: ")
 
         ticker = yf.Ticker(symbol)
 
         image_url = ticker.get_info()["logo_url"]
         st.image(image=image_url)
 
-        st.write(ticker.get_info()["shortName"])
+        # Introduces a bunch of variables that will be used in the following lines of code.
+        sector = ticker.get_info()["sector"]
+        industry = ticker.get_info()["industry"]
+        ticker_name = ticker.get_info()["shortName"]
+        beta = ticker.info["beta"]
+        market_cap = ticker.get_info()["marketCap"]
 
+        # To overcome the error, if a company doesn't pay out any dividends, it will simply return zero.
+        try:
+            dividend = (ticker.dividends[-1])
+        except IndexError:
+            dividend = 0
+
+        # This chunk of code uses the variables we previously created above.
+        st.write(ticker_name)
+        st.markdown(f"**{symbol}** is in the '{sector}' sector and '{industry}' industry.")
+        st.markdown(f"It has a **beta** of {beta} and a **market cap** of ${market_cap:,}!")
+        st.markdown(f"Current dividends are: ${dividend}.")
         website = ticker.get_info()["website"]
         with st.expander("Associated Website"):
             st.write(website)
 
+        # A few variables needed for the following lines.
         price = (ticker.get_info()["currentPrice"])
         recc = (ticker.get_info()["recommendationKey"])
 
-        st.subheader(f"The current price of {symbol} is ${price}.")
-        st.subheader(f"It's current recommendation is {recc}!")
+        st.subheader(f"{symbol} has a current price of : ${price}.")
+        st.markdown(f"### Recommendation: {recc.upper()}!")
 
+        st.markdown("""
+        ---
+        """)
+
+        # Returns the closing stock price from the last seven days, or 5 trading days.
+        seven_days = ticker.history(period="1h", start=(dt.datetime.now().date() - dt.timedelta(days=7)))
+
+        # This code helps generate the 5 previous closing prices.
+        st.subheader(f"Previous 5 closing prices for {ticker_name}:")
+        for i in seven_days.Close:
+            st.write(f"${round(i,2)}")
+
+        st.markdown("""
+        ---
+        """)
+
+        # Created 2 columns for start and end date.
         col1, col2 = st.columns(2)
 
         with col1:
             start = st.date_input("What is the start date?: ",
-                                  value=(dt.datetime.now().date() - dt.timedelta(days=90)))
+                                  value=(dt.datetime.now().date() - dt.timedelta(days=30)))
 
         with col2:
             end = st.date_input("What is the end date?: ")
 
+        # This block of code is ideally for a variety of parameters.
+        st.write("Choose a parameter:")
+        selector = st.checkbox(label="Volume")
+
         ticker_history = ticker.history(period="1d", start=start, end=end)
 
-        col3, col4 = st.columns(2)
-
-        with col3:
-            selector = st.selectbox(label="Choose a parameter:", options=["Open", "Close", "Volume"])
-
-        with col4:
-            selector2 = st.selectbox(label="Additional viewing options:", options=["Coming Soon"])
-
-        if selector == "Open":
-            st.line_chart(ticker_history.Open)
-        elif selector == "Close":
-            st.line_chart(ticker_history.Close)
-        elif selector == "Volume":
+        # This code block is dependent upon the number of parameters created.
+        if selector == True:
             st.line_chart(ticker_history.Volume)
         else:
             st.write("Awaiting input...")
